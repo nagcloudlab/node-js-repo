840b566e1a3200b494b9b1d9a2988f594f4e9116
Nag <nagcloudlab@gmail.com>
2022-03-25 17:07:48 +0530

    sessions rest api

--- 3-node.js/1-Level-1/1-blocking-IO.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const vegMenu = fs.readFileSync('./veg-menu.txt') // 6s
-console.log(vegMenu.toString('utf8'))
-
-const nonVegMenu = fs.readFileSync('./non-veg-menu.txt') // 4s
-console.log(nonVegMenu.toString('utf8'))
-
-console.log("do something else..")
