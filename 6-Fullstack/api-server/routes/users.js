7a6c184cb059c523d6b337a8da2e5f7b99b6ce80
Nag <nagcloudlab@gmail.com>
2022-03-30 11:45:26 +0530

    node mongodb

---
+++ 6-Fullstack/api-server/routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri =
+  "mongodb://localhost:27017";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+router
+  .post('/', async function (req, res, next) {
+    const reqBody = req.body
+    try {
+      await client.connect();
+      const database = client.db('shopdb');
+      const users = database.collection('users');
+      const user = await users.insertOne({ ...reqBody });
+      console.log(user);
+      res.status(201).json(user)
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  })
+  .get('/', async function (req, res, next) {
+    try {
+      await client.connect();
+      const database = client.db('shopdb');
+      const users = database.collection('users');
+      const cursor = await users.find({});
+      res.status(200).json(await cursor.toArray())
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  })
+
+module.exports = router;
